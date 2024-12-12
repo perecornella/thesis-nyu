607804902619ed75fe697abd4dccfe6d04b2ef0e
Pere Cornellà Franch <perecornella@10-17-225-226.dynapool.wireless.nyu.edu>
2024-12-12 15:30:04 -0500

    Organized ernich_dataset.py and added buttons to change datachannel and visualization.

--- enrich_dataset.py
+++ enrich_dataset.py
@@ -17,22 +17,23 @@ class Canvas(FigureCanvas):
         super().__init__(fig)  # Pass the figure to the FigureCanvas constructor
         self.setParent(parent)  # Set the parent for the canvas
 
+
 class AppDemo(QWidget):
     def __init__(self, input_directory=None, input_file=None):
 
         super().__init__()
+
         self.input_directory = input_directory
         self.input_file = input_file
 
-        # Create the main layout
-        self.layout = QVBoxLayout()
+        self.set_directory()
+        self.load_data_on_checkpoint()
+        self.create_widgets()
+        self.set_layout()
 
-        # Create QButtonGroup instances for each category
-        self.tuned_button_group = QButtonGroup(self)
-        self.clear_button_group = QButtonGroup(self)
-        self.healthy_button_group = QButtonGroup(self)
 
-        # Initialize with the first sample
+    def set_directory(self):
+
         all_done = True
         for i, row in dir_info.iterrows():
             if row['end'] - row['checkpoint'] > 0:
@@ -47,9 +48,9 @@ class AppDemo(QWidget):
             metadata.to_csv(f'metadata/{datachannel}.csv')
             sys.exit(1)
 
-        if input_directory is not None:
+        if self.input_directory is not None:
             try:
-                row = dir_info[dir_info['name'] == input_directory].iloc[0]
+                row = dir_info[dir_info['name'] == self.input_directory].iloc[0]
                 self.checkpoint = row['checkpoint']
                 self.end = row['end']
                 self.dir = row['name']
@@ -61,6 +62,8 @@ class AppDemo(QWidget):
         self.number_of_batches = (self.end - self.checkpoint) // self.batch_size
         self.batch_pointer = 0
         self.counter = 0
+
+    def load_data_on_checkpoint(self):
 
         all_error = True
         while self.batch_pointer < self.number_of_batches:
@@ -111,27 +114,62 @@ class AppDemo(QWidget):
             dir_info.to_csv('metadata/dir_info.csv')
             metadata.to_csv(f'metadata/{datachannel}.csv')
             sys.exit(1)
-        
-        # Add the initial dashboard (Canvas) to the layout
-        self.chart = Canvas(self, self.sample, self.filename)
-        self.layout.addWidget(self.chart)
 
-        # Create horizontal layouts for Yes/No buttons
+    def create_widgets(self):
+
+        # FRA or Traces question for Visualization
+        self.visualization_combobox = QComboBox()
+        self.visualization_combobox.addItems(["FRA", "Traces"])
+        self.visualization_layout = QHBoxLayout()
+        self.visualization_layout.addWidget(QLabel("Visualization"))
+        self.visualization_layout.addWidget(self.visualization_combobox)
+
+        # Channel 0 or 2 question for Channel
+        self.channel_combobox = QComboBox()
+        self.channel_combobox.addItems(["Channel 0", "Channel 2"])
+        self.channel_layout = QHBoxLayout()
+        self.channel_layout.addWidget(QLabel("Channel"))
+        self.channel_layout.addWidget(self.channel_combobox)
+
+        # Tuned button
+        self.tuned_button_group = QButtonGroup(self)
         self.tuned_layout = QHBoxLayout()
-        self.clear_layout = QHBoxLayout()
-        self.healthy_layout = QHBoxLayout()
-
-        # Yes/No buttons for Tuned, clear, Healthy
         self.tuned_button_yes = QRadioButton("Yes")
         self.tuned_button_no = QRadioButton("No")
+        self.tuned_layout.addWidget(QLabel("Tuned"))
+        self.tuned_layout.addWidget(self.tuned_button_yes)
+        self.tuned_layout.addWidget(self.tuned_button_no)
+        self.tuned_button_group.addButton(self.tuned_button_yes)
+        self.tuned_button_group.addButton(self.tuned_button_no)
+
+        # Clear button
+        self.clear_button_group = QButtonGroup(self)
+        self.clear_layout = QHBoxLayout()
         self.clear_button_yes = QRadioButton("Yes")
         self.clear_button_no = QRadioButton("No")
+        self.clear_layout.addWidget(QLabel("Clear"))
+        self.clear_layout.addWidget(self.clear_button_yes)
+        self.clear_layout.addWidget(self.clear_button_no)
+        self.clear_button_group.addButton(self.clear_button_yes)
+        self.clear_button_group.addButton(self.clear_button_no)
+
+        # Healthy button
+        self.healthy_button_group = QButtonGroup(self)
+        self.healthy_layout = QHBoxLayout()
         self.healthy_button_yes = QRadioButton("Yes")
         self.healthy_button_no = QRadioButton("No")
+        self.healthy_layout.addWidget(QLabel("Healthy"))
+        self.healthy_layout.addWidget(self.healthy_button_yes)
+        self.healthy_layout.addWidget(self.healthy_button_no)
+        self.healthy_button_group.addButton(self.healthy_button_yes)
+        self.healthy_button_group.addButton(self.healthy_button_no)
 
         # Intra/Extra question for Type
         self.type_combobox = QComboBox()
         self.type_combobox.addItems(["Intra", "Extra"])
+        self.type_layout = QHBoxLayout()
+        self.type_layout.addWidget(QLabel("Type"))
+        self.type_layout.addWidget(self.type_combobox)
 
         # Coordinates input for x, y, z
         self.x0_input = QLineEdit()
@@ -140,40 +178,6 @@ class AppDemo(QWidget):
         self.n_input = QLineEdit()
         self.y_input = QLineEdit()
         self.z_input = QLineEdit()
-
-        # Add the radio buttons horizontally to the layouts
-        self.tuned_layout.addWidget(QLabel("Tuned"))
-        self.tuned_layout.addWidget(self.tuned_button_yes)
-        self.tuned_layout.addWidget(self.tuned_button_no)
-
-        self.clear_layout.addWidget(QLabel("Clear"))
-        self.clear_layout.addWidget(self.clear_button_yes)
-        self.clear_layout.addWidget(self.clear_button_no)
-
-        self.healthy_layout.addWidget(QLabel("Healthy"))
-        self.healthy_layout.addWidget(self.healthy_button_yes)
-        self.healthy_layout.addWidget(self.healthy_button_no)
-
-        # Add radio buttons to the button groups to ensure only one is selected at a time
-        self.tuned_button_group.addButton(self.tuned_button_yes)
-        self.tuned_button_group.addButton(self.tuned_button_no)
-        self.clear_button_group.addButton(self.clear_button_yes)
-        self.clear_button_group.addButton(self.clear_button_no)
-        self.healthy_button_group.addButton(self.healthy_button_yes)
-        self.healthy_button_group.addButton(self.healthy_button_no)
-
-        # Add the layouts to the main layout
-        self.layout.addLayout(self.tuned_layout)
-        self.layout.addLayout(self.clear_layout)
-        self.layout.addLayout(self.healthy_layout)
-
-        # Add Type ComboBox in a horizontal layout
-        self.type_layout = QHBoxLayout()
-        self.type_layout.addWidget(QLabel("Type"))
-        self.type_layout.addWidget(self.type_combobox)
-        self.layout.addLayout(self.type_layout)
-
-        # Add Coordinates inputs in a horizontal layout
         self.coord_layout = QHBoxLayout()
         self.coord_layout.addWidget(QLabel("Coordinates"))
         self.coord_layout.addWidget(QLabel("x0:"))
@@ -188,47 +192,56 @@ class AppDemo(QWidget):
         self.coord_layout.addWidget(self.y_input)
         self.coord_layout.addWidget(QLabel("z:"))
         self.coord_layout.addWidget(self.z_input)
-        self.layout.addLayout(self.coord_layout)
 
         # Add Send button
         self.send_button = QPushButton("Send")
         self.send_button.clicked.connect(self.on_send_clicked)
 
-        # Add the Send button to the layout
-        self.layout.addWidget(self.send_button)
+    def set_layout(self):
 
-        # Set the layout for the QWidget
+        self.layout = QVBoxLayout()
+
+        self.layout.addLayout(self.visualization_layout)
+        self.layout.addLayout(self.channel_layout)
+        self.chart = Canvas(self, self.sample, self.filename)
+        self.layout.addWidget(self.chart)
+        self.layout.addLayout(self.tuned_layout)
+        self.layout.addLayout(self.clear_layout)
+        self.layout.addLayout(self.healthy_layout)
+        self.layout.addLayout(self.type_layout)
+        self.layout.addLayout(self.coord_layout)
+        self.layout.addWidget(self.send_button)
+    
         self.setLayout(self.layout)
+
+# Action functions
 
     def on_send_clicked(self):
         if self.input_file is not None:
             QMessageBox.warning(self, "Warning", "You inserted a filename; changes cannot be persisted.")
 
         else:
-            # Get the selected Yes/No values
             tuned = "Yes" if self.tuned_button_yes.isChecked() else "No"
             clear = "Yes" if self.clear_button_yes.isChecked() else "No"
             healthy = "Yes" if self.healthy_button_yes.isChecked() else "No"
-
-            # Get the selected Type
             selected_type = self.type_combobox.currentText()
+            x0 = float(self.x0_input.text()) if self.x0_input.text() else None
+            xf = float(self.xf_input.text()) if self.xf_input.text() else None
+            i = float(self.i_input.text()) if self.i_input.text() else None
+            n = float(self.n_input.text()) if self.n_input.text() else None
+            y = float(self.y_input.text()) if self.y_input.text() else None
+            z = float(self.z_input.text()) if self.z_input.text() else None
 
-            # Get the coordinates
-            x0 = float(self.x0_input.text())
-            xf = float(self.xf_input.text())
-            i = float(self.i_input.text())
-            n = float(self.n_input.text())
-            y = float(self.y_input.text())
-            z = float(self.z_input.text())
+            if None not in [x0, xf, i, n]:
+                x = x0 + i * (xf - x0) // n
+            else:
+                x = None
 
-            # Prepare the message for the confirmation pop-up
             confirmation_message = f"Tuned: {tuned}\nClear: {clear}\nHealthy: {healthy}\nType: {selected_type}\nCoordinates: x={x0 + i * (xf - x0) // n}, y={y}, z={z}"
-
-            # Show the confirmation pop-up
-            reply = QMessageBox.question(self, 'Confirm your entries', f"Are you sure you want to submit the following values?\n\n{confirmation_message}", 
+            question = QMessageBox.question(self, 'Confirm your entries', f"Are you sure you want to submit the following values?\n\n{confirmation_message}", 
                                         QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
 
-            if reply == QMessageBox.Yes:
+            if question == QMessageBox.Yes:
                 new_row = {
                     'directory': self.dir,
                     'filename': f"A{self.checkpoint:03d}",
@@ -236,7 +249,7 @@ class AppDemo(QWidget):
                     'clear': clear,
                     'healthy': healthy,
                     'type': selected_type,
-                    'x': x0 + i * (xf - x0) // n,
+                    'x': x,
                     'y': y,
                     'z': z,
                     'd': self.d,
@@ -245,86 +258,34 @@ class AppDemo(QWidget):
                     'entrydate': datetime.now()
                 }
 
-                # Add the new row to the DataFrame
                 metadata.loc[len(metadata)] = new_row
-
-
-                # If user clicked Yes, proceed with sending the data
-                # print(f"Confirmed: {confirmation_message}")
                 self.checkpoint += 1
-                all_error = True
-                while self.batch_pointer < self.number_of_batches:
-                    if self.counter == 0:
-                        if self.end - self.checkpoint > 10:
-                            batch = [self.checkpoint, self.checkpoint + 10]
-                        else:
-                            batch = [self.checkpoint, self.end]
-
-                        self.data, self.tonedata, self.error_files = read_in_data(root_dir + self.dir, batch, datachannel, triggerchannel)
-                        self.df = pd.merge(self.data, self.tonedata, how="left", on="toneid")
-                        
-                        if len(self.error_files) - (batch[1] - batch[0]) == 0:  # All files are error
-                            self.batch_pointer += 1
-                            self.checkpoint += batch[1] - batch[0]
-                            dir_info.loc[dir_info['name'] == self.dir, 'checkpoint'] = self.checkpoint
-                            break
-                        else:
-                            self.counter = (batch[1] - batch[0]) - len(self.error_files)
-                    
-                    else:
-                        while self.checkpoint < self.end:
-                            if f"A{self.checkpoint:03d}" not in self.error_files:
-                                self.sample = self.df[self.df["toneid"].str.startswith(f"A{self.checkpoint:03d}")]
-                                self.filename = f"{self.dir}A{self.checkpoint:03d} channel {datachannel}"
-                                all_error = False
-                                self.counter -= 1
-                                break
-                            else:
-                                self.checkpoint += 1
-                
-                        dir_info.loc[dir_info['name'] == self.dir, 'checkpoint'] = self.checkpoint
-                        if not all_error:
-                            break
-                                
-                if all_error:
-                    print(f"All the remaining non-error files in {self.dir} have been processed.")
-                    dir_info.to_csv('metadata/dir_info.csv')
-                    metadata.to_csv(f'metadata/{datachannel}.csv')
-                    sys.exit(1)
-
+                self.load_data_on_checkpoint()
                 self.update_dashboard()
+
             else:
-                # If user clicked No, just print the message
                 print("Submission cancelled.")
 
     def update_dashboard(self):
         plt.close(self.chart.figure)
-        
-        # Remove the old canvas
         self.layout.removeWidget(self.chart)
         self.chart.deleteLater()
-
-        # Create a new canvas with updated data
         self.chart = Canvas(self, self.sample, self.filename)
         self.layout.insertWidget(0, self.chart)
 
     def closeEvent(self, event):
-        # Create a message box asking for confirmation
-        reply = QMessageBox.question(self, 'Confirm Exit',
+        question = QMessageBox.question(self, 'Confirm Exit',
                                     "Do you want to save changes before exiting?",
                                     QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel, 
                                     QMessageBox.Yes)
 
-        if reply == QMessageBox.Yes:
-            # If the user selects Yes, save the files
+        if question == QMessageBox.Yes:
             dir_info.to_csv('metadata/dir_info.csv')
             metadata.to_csv(f'metadata/{datachannel}.csv')
             event.accept()  # Close the window
-        elif reply == QMessageBox.No:
-            # If the user selects No, close without saving
+        elif question == QMessageBox.No:
             event.accept()
         else:
-            # If the user selects Cancel, do nothing (keep the window open)
             event.ignore()
 
 
